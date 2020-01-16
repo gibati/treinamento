1704851572156d0dfb418d0d22025743b3b6aa55
Gilberto A Lima <gilberto.lima@easynvest.com.br>
2020-01-15 23:33:36 -0300

    Inclusao de validacao para o exercicio 4

--- gibati/linux-essentials/scripts/desafio_validate_environment.sh
+++ gibati/linux-essentials/scripts/desafio_validate_environment.sh
@@ -6,6 +6,8 @@
 cod_fail="\033[05;31m [FAIL] \033[00;37m"
 cod_ok="\033[05;32m [OK]  \033[00;37m"
 dir_desafio="/opt/desafio/1"
+user1="maria"
+user2="jose"
 file_1="user1.conf"
 file_2="user2.conf"
 
@@ -17,15 +19,32 @@ else
 fi 
 
 # Exercicio2
-if [ -e $file_1 ] && [ -e $file_2 ]; then
+if [ -e $dir_desafio/$file_1 ] && [ -e $dir_desafio/$file_2 ]; then
     echo -e "Exercício 2-$cod_ok"
 else
   echo -e "Exercício 2-$cod_fail"
 fi 
 
 # Exercicio3
-if [ $(grep maria $dir_desafio/$file_1 >/dev/null) ] && [ $(grep jose $dir_desafio/$file_2 >/dev/null) ]; then
+if [ $(grep $user1 $dir_desafio/$file_1 2>/dev/null) ] && [ $(grep $user2 $dir_desafio/$file_2 2>/dev/null) ]; then
   echo -e "Exercício 3-$cod_ok"
 else
   echo -e "Exercício 3-$cod_fail"
+fi
+
+# Exercicio4
+id $user1 >/dev/null 2>&1
+return_1=$(echo $?)
+id $user2 >/dev/null 2>&1
+return_2=$(echo $?)
+if [ $return_1 = 0 ] && [ $return_2 = 0 ]; then
+  return_permission_1=$(find $dir_desafio -user $user1 -name $file_1 | wc -l)
+  return_permission_2=$(find $dir_desafio -user $user2 -name $file_2 | wc -l)
+  if [ $return_permission_1 = 1 ] && [ $return_permission_2 = 1 ]; then
+    echo -e "Exercício 4-$cod_ok"
+  else
+    echo -e "Exercício 4-$cod_fail"
+  fi
+else
+  echo -e "Exercício aqui4-$cod_fail"
 fi
