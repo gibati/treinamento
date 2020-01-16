6b69ff9f8cc53f0fcafba01b63c1405b04ae8b10
Gilberto A Lima <gilberto.lima@easynvest.com.br>
2020-01-16 01:44:55 -0300

    Ajustes criação do ambiente de testes

--- gibati/linux-essentials/scripts/desafio_create_environment.sh
+++ gibati/linux-essentials/scripts/desafio_create_environment.sh
@@ -3,14 +3,16 @@
 #
 # create environment for learning exercises
 
-dir_desafio="/opt/desafio/1"
-
-# Cria arquivo gigante
-dd if=/dev/zero of=/arquivo_gigante.log bs=1M count=5000
-
-# Cria arquivo oculto
-if [ -d $dir_desafio ]; then
-  echo "Me achou, pode remover" > /opt/desafio/1/.ME-ACHOU.log
-else
-  echo "Diretorio $dir_desafio não criado na máquina"
-fi
+ec2_tag_filter="treinamento"
+aws_credentials="/opt/chaves/AWS_Keypairs/treinamento_linux.pem"
+ssh_parametros="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+for ips in \
+  $(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ec2_tag_filter" | jq -r '.Reservations[].Instances[].PrivateIpAddress'); do
+  echo "Ec2 IpAddress: "$ips"";
+  for i in $ips; do
+    ssh -i $aws_credentials $ssh_parametros ubuntu@$i -C "sudo dd if=/dev/zero of=/arquivo_gigante.log bs=1M count=3500 & " 2>/dev/null
+    ssh -i $aws_credentials $ssh_parametros ubuntu@$i -C "sudo mkdir -p /opt/desafio/1" 2>/dev/null
+    ssh -i $aws_credentials $ssh_parametros ubuntu@$i -C "sudo touch /opt/desafio/1/.ME-ACHOU.log" 2>/dev/null
+    echo "----------------------------"
+  done
+done
